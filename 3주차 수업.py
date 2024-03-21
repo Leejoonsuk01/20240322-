32c0e20aa939adcb85703ef327e854f65abe213f
Leejoonsuk01 <163505582+Leejoonsuk01@users.noreply.github.com>
2024-03-22 00:38:28 +0900

    2118314 이준석
    컴퓨터 프로그래밍 수업 3주차 퀴즈

---
+++ 3주차 수업.py
@@ -0,0 +1,41 @@
+name = ("이준석")
+print(name)
+
+a = b ="bee"
+print(a, b)
+
+a ='그는 말했다."밥먹자"'
+print(a)
+
+a = 1
+b = 2
+print(a + b)
+
+print("-"*50)
+
+print("사과"*50)
+
+a = "사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과사과"
+len(a)
+
+string_a = "가나다라마바사"
+print(string_a[0:4])
+number = 1
+type(number)
+
+a = 4
+b = 2
+
+print(a + b,a -b, a * b )
+
+a = ["배열 1번", 2, "배열 3번"]
+
+a = [ 1,2,3]
+print(a[0:2])
+
+a= [ 1,2,3,4,5]
+a.remove(3)
+print(a)
+
+person = {"이름" : "이준석", "나이" :"24"}
+print(person)
